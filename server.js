3b9691b573f5b5e86b899b5817e990dd909b00d9
Siddarth Bisht <ssbdel97@gmail.com>
2018-01-13 23:20:00 +0530

    Add POST for TODOS

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 var express=require('express');
+var bodyParser = require('body-parser');
 var app =express();
 var PORT=process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description:'Meet Mom for lunch',
-	completed:false
-},{
-	id: 2,
-	description:'Go to Market',
-	completed:false
-},{
-	id: 3,
-	description:'Feed the cat',
-	completed:true
-}];
+app.use(bodyParser.json());
+// var todos = [{
+// 	id: 1,
+// 	description:'Meet Mom for lunch',
+// 	completed:false
+// },{
+// 	id: 2,
+// 	description:'Go to Market',
+// 	completed:false
+// },{
+// 	id: 3,
+// 	description:'Feed the cat in the evening',
+// 	completed:true
+// }];
 
+var todos=[];
+var todoNextId=1;
 app.get('/',function(req,res){
 	res.send('Todo API Root');
 });
@@ -42,6 +46,13 @@ app.get('/todos/:id',function(req,res){
 			//res.send(found);
 			res.json(found);
 	}
+});
+app.post('/todos',function(req,res){
+	var body = req.body;
+	body.id=todoNextId++;
+	//console.log('description '+body.description);
+	todos.push(body);
+	res.json(body);
 });
 app.listen(PORT,function(){
 	console.log('Express Listening on PORT '+PORT +'!');
