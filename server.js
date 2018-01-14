f60f1d7bb4f0152499ad6c14c80df27686b39336
Siddarth Bisht <ssbdel97@gmail.com>
2018-01-15 00:09:16 +0530

    Final Static todos

--- server.js
+++ server.js
@@ -19,6 +19,11 @@ app.get('/todos',function(req,res){
 	}else if(queryParams.hasOwnProperty('completed')&&queryParams.completed==='false'){
 		filteredTodos=_.where(filteredTodos,{completed:false});
 	}
+	if(queryParams.hasOwnProperty('q')&&queryParams.q.length >0){
+		filteredTodos = _.filter(filteredTodos,function(todo){
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		});
+	}
 	res.json(filteredTodos);
 });
 app.get('/todos/:id',function(req,res){
