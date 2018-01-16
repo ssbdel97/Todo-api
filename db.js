301b12d04b03d1a45eb11e4a91ab7163640154a0
Siddarth Bisht <ssbdel97@gmail.com>
2018-01-16 23:17:19 +0530

    Add postgres

--- db.js
+++ db.js
@@ -1,10 +1,19 @@
 var Sequelize = require('sequelize');
-var sequelize =new Sequelize(undefined,undefined,undefined,{
-	'dialect' : 'sqlite',
-	'storage' : __dirname +'/data/dev-todo-api.sqlite'
-});
+var env = process.env.NODE_ENV || 'development';
+var sequelize;
+
+if (env == 'production') {
+	sequelize = new Sequelize(process.env.DATABASE_URL, {
+		dialect: 'postgres'
+	});
+} else {
+	sequelize = new Sequelize(undefined, undefined, undefined, {
+		'dialect': 'sqlite',
+		'storage': __dirname + '/data/dev-todo-api.sqlite'
+	});
+}
 var db = {};
-db.todo=sequelize.import(__dirname + '/model/todo.js');
+db.todo = sequelize.import(__dirname + '/model/todo.js');
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-module.exports =db;
+module.exports = db;
