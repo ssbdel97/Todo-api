f8b49debfd8b15d63fec9f6b5f692eeaf05a83df
Siddarth Bisht <ssbdel97@gmail.com>
2018-01-16 23:25:16 +0530

    Added postgres2

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ var Sequelize = require('sequelize');
 var env = process.env.NODE_ENV || 'development';
 var sequelize;
 
-if (env == 'production') {
+if (env === 'production') {
 	sequelize = new Sequelize(process.env.DATABASE_URL, {
 		dialect: 'postgres'
 	});
