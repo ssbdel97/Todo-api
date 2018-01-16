c5e324b3886f36e88904f7a1ff558f3db762eb01
Siddarth Bisht <ssbdel97@gmail.com>
2018-01-17 00:29:22 +0530

    another update

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ var sequelize;
 
 if (env === 'production') {
 	sequelize = new Sequelize(process.env.DATABASE_URL, {
-		dialect: 'postgres'
+		'dialect': 'postgres'
 	});
 } else {
 	sequelize = new Sequelize(undefined, undefined, undefined, {
