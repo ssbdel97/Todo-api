44b5c7a4a54ecad117d12d1b5bf420fe18fae998
Siddarth Bisht <ssbdel97@gmail.com>
2018-01-19 20:46:45 +0530

    Add auth token generation to user login

--- server.js
+++ server.js
@@ -161,14 +161,22 @@ app.post('/users', function(req, res) {
 });
 app.post('/users/login', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
-	db.user.authenticate(body).then(function(user){
-		res.json(user.toPublicJSON());
-	},function(){
+	db.user.authenticate(body).then(function(user) {
+		var token = user.generateToken('authentication');
+		if (token) {
+			res.header('Auth', token).json(user.toPublicJSON());
+		} else {
+			res.status(401).send()
+
+		}
+	}, function() {
 		res.status(401).send()
 	});
 
 });
-db.sequelize.sync({force: true}).then(function() {
+db.sequelize.sync({
+	force: true
+}).then(function() {
 
 	app.listen(PORT, function() {
 		console.log('Express Listening on PORT ' + PORT + '!');
